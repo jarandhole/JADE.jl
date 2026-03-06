b542210320c43052f3b7329dd95d43008726d8fa
Jarand Hole <jho@nve.no>
2026-03-06 13:18:00 +0100

    fix

--- src/model.jl
+++ src/model.jl
@@ -371,12 +371,12 @@ function JADEsddp(d::JADEData, optimizer = nothing)
                     # Investment version: defining capacity constraints for wind
                     useWind[m in s.WINDS, bl in s.BLOCKS],
                     new_wind_gen[m, bl] <=
-                    d.wind_representation[m][(timenow.year,timenow.week, bl)] * invested_capacity[string(m)].in
+                    d.wind_representation[m][(timenow.year,timenow.week, bl)] * invested_capacity[m].in
 
                     # Investment version: defining capacity constraints for solar
                     useSolar[m in s.SOLARS, bl in s.BLOCKS],
                     new_solar_gen[m, bl] <=
-                    d.solar_representation[m][(timenow.year,timenow.week, bl)] * invested_capacity[string(m)].in
+                    d.solar_representation[m][(timenow.year,timenow.week, bl)] * invested_capacity[m].in
 
                     # Thermal plant capacities
                     useThermal[m in s.THERMALS, bl in s.BLOCKS],
