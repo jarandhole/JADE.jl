bc31ecad65bf5fd63193b2efcbe616cf1d59a477
Jarand Hole <jho@nve.no>
2026-03-07 18:42:07 +0100

    bugfix

--- src/model.jl
+++ src/model.jl
@@ -156,8 +156,8 @@ function JADEsddp(d::JADEData, optimizer = nothing)
                 )
             end
 
-            JuMP.@constraint(md, defineShedding, 1 == 1)
-            JuMP.@constraint(md, rbalance, 1 == 1)
+            JuMP.@constraint(md, defineShedding[n in s.NODES, bl in s.BLOCKS], 1 == 1) # Investment version: dummy constraint
+            JuMP.@constraint(md, rbalance[r in s.RESERVOIRS], 1 == 1) # Investment version: dummy constraint
 
             # Stage objective set to cost of investments (ajusted for reinvestment in steady state)
             if d.rundata.steady_state 
