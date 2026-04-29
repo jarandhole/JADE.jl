b7178b66b44a4a371b341e4c08164cf24a9f92be
Jarand Hole <jho@nve.no>
2026-04-29 15:03:32 +0200

    bf

--- src/data/thermal.jl
+++ src/data/thermal.jl
@@ -119,11 +119,11 @@ function getfuelcosts(filename::String)
     while (ret = iterate(rows, row_state)) !== nothing
         row, row_state = ret
         time = TimePoint(parse(Int, row.Column1), parse(Int, row.Column2))
-        if isempty(data)
-            start_time = time
-        elseif time != start_time + length(data)/5 # Trying to devide for num_blocks, TODO: make it more robust 
-            error("Weeks in $filename must be contiguous")
-        end
+        #if isempty(data)
+        #    start_time = time
+        #elseif time != start_time + length(data)/5 # Trying to devide for num_blocks, TODO: make it more robust 
+        #    error("Weeks in $filename must be contiguous")
+        #end
         d = Dict{NTuple{2,Symbol},Float64}(
             (str2sym("$k"), str2sym(row[:Column3])) => parse(Float64, row[k]) for
             k in CSV.getnames(row) if !(k in (:Column1, :Column2, :Column3))
