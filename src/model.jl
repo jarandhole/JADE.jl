4071871b7ed99ab4474c6f6d265f6d872717cd8f
Jarand Hole <jho@nve.no>
2026-05-12 10:53:49 +0200

    bf

--- src/model.jl
+++ src/model.jl
@@ -574,8 +574,8 @@ function JADEsddp(d::JADEData, optimizer = nothing)
 
 
             SDDP.parameterize(md, inflow_uncertainty) do ϕ 
-            #y = 1991
-            #j = 2023
+                y = 0
+                j = 0
                 for (c, value) in ϕ
                 if c == :scenario
                     if value < 1000
