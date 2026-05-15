36716144d8dde5d445270beb5095f5bc8e0507ef
Jarand Hole <jho@nve.no>
2026-05-15 16:32:59 +0200

    testing to check if InSampleMonteCarlo sampling will work

--- src/solve.jl
+++ src/solve.jl
@@ -270,13 +270,13 @@ function optimize_policy!(
                 sddpm,
                 iteration_limit = solveoptions.iterations,
                 cut_deletion_minimum = solveoptions.cutselection,
-                sampling_scheme = SDDP.Historical(sample_paths; terminate_on_cycle = true),
+                sampling_scheme = SDDP.InSampleMonteCarlo(), #SDDP.Historical(sample_paths; terminate_on_cycle = true),
                 cycle_discretization_delta = 10.0,
                 dashboard = false,
                 risk_measure = solveoptions.riskmeasure,
                 parallel_scheme = parallel_scheme,
                 print_level = print_level,
-                forward_pass = SDDP.DefaultForwardPass(; include_last_node = false),
+                forward_pass = SDDP.DefaultForwardPass(), #(; include_last_node = false),
             )
         else
             solveresults = SDDP.train(
