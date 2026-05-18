8d960358ef5dde13ffe676410bd5a7adc80a9adf
Jarand Hole <jho@nve.no>
2026-05-18 13:42:47 +0200

    bugfix

--- src/solve.jl
+++ src/solve.jl
@@ -260,22 +260,21 @@ function optimize_policy!(
 
         for sample_path in sample_paths
             println("Looking at samplepath with length $(length(sample_path))")
+            new_sample_path = Tuple{Int,Dict{Symbol,Float64}}[]
             for (t, inflows) in sample_path
-                new_sample_path = Tuple{Int,Dict{Symbol,Float64}}[]
                 if t == 1
                     push!(new_sample_path, (t, inflows))
                 else
                     for i in 1:3
-                        push!(new_sample_path, (t + i*52, inflows))
+                        push!(new_sample_path, (t + (i-1)*52, inflows))
                     end
                 end
-                println("Generated new sample path with length $(length(new_sample_path))")
                 push!(new_sample_paths, new_sample_path)
             end
-
+            println("Finished with new samplepath with length $(length(new_sample_path))")
         end 
 
-        println("Made it with $(length(new_sample_paths)) sample paths for training using the $(method) method.")
+        println("Made it with $(length(new_sample_paths)) sample paths for training.")
         println("Each sample path contains $(length(new_sample_paths[1])) stages.")
         println("Each stage contains inflows for the following catchments: $(keys(new_sample_paths[1][1][2]))")
         println("sample path example: $(new_sample_paths[1])")
