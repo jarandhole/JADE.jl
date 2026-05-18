624bccbbd1d4179c02a1092e111358d0f04a0d73
Jarand Hole <jho@nve.no>
2026-05-18 12:47:30 +0200

    terminate on cycle false

--- src/solve.jl
+++ src/solve.jl
@@ -277,13 +277,13 @@ function optimize_policy!(
                 sddpm,
                 iteration_limit = solveoptions.iterations,
                 cut_deletion_minimum = solveoptions.cutselection,
-                sampling_scheme = SDDP.Historical(sample_paths; terminate_on_dummy_leaf = true), # terminating on dummy leaf, document whats happening
+                sampling_scheme = SDDP.Historical(sample_paths; terminate_on_cycle = false), # terminating on dummy leaf, document whats happening
                 cycle_discretization_delta = 10.0,
                 dashboard = false,
                 risk_measure = solveoptions.riskmeasure,
                 parallel_scheme = parallel_scheme,
                 print_level = print_level,
-                forward_pass = SDDP.DefaultForwardPass(;  include_last_node = false), #when terminate_on_cycle (;  include_last_node = false)
+                forward_pass = SDDP.DefaultForwardPass() #when terminate_on_cycle (;  include_last_node = false)
             )
         else
             solveresults = SDDP.train(
