73abfd96a7f5c6ed45a2060786c094d20dbd34fe
Jarand Hole <jho@nve.no>
2026-06-10 15:45:53 +0200

    updated getwindrepresentation

--- src/data/wind.jl
+++ src/data/wind.jl
@@ -43,7 +43,7 @@ function getwindrepresentation(filename::String)
         # Read each subsequent line
         for line in eachline(file)
             row = split(chomp(line), ",")
-            station = Symbol(row[1])
+            station = str2sym(row[1])
             year = parse(Int, row[2])
             week = parse(Int, row[3])
 
