ab8fc0f5bc0502be7f8b5bf56a880102e6a5ac42
Jarand Hole <jho@nve.no>
2026-06-11 11:45:54 +0200

    remove print

--- src/model.jl
+++ src/model.jl
@@ -92,9 +92,6 @@ function JADEsddp(d::JADEData, optimizer = nothing)
         # Investment version: State variables: invested capacities
         #------------------------------------------------------------------------
         
-        println("s.TRANS_ARCS ")
-        println(s.TRANS_ARCS)
-
         JuMP.@variable(
             md,
             0 <=
